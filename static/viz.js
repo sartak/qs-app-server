f28d590c6219497537cfc1b3dac9766be11115e3
Shawn M Moore <code@sartak.org>
2017-11-15 00:46:04 +0000

    Load all events from the database server

--- static/viz.js
+++ static/viz.js
@@ -1,2 +1,43 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function loadEvents (progress, complete, failure, url) {
+    jQuery.ajax({
+        url: url || "/api/events?fields=timestamp",
+        cache: false,
+        headers: {
+            'X-QS-Username': getParameterByName('user'),
+            'X-QS-Password': getParameterByName('pass')
+        },
+        error: function (xhr, description, error) {
+            failure(description, error);
+        },
+        success: function (data) {
+            if (data.results && data.results.length) {
+                progress(data.results);
+            }
+
+            if (data.nextPage) {
+                loadEvents(progress, complete, failure, data.nextPage);
+            }
+            else {
+                complete();
+            }
+        }
+    });
+}
+
 jQuery(function () {
+    loadEvents(function (events) {
+    }, function () {
+    }, function (description, error) {
+        alert(description + " " + error);
+    });
 });
